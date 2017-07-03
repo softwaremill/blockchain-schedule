b1ce61816ee187281826e8529c14842afb82e0bc
Krzysiek Ciesielski <kciesielski@users.noreply.github.com>
2017-07-03 13:19:21 +0200

    Configure frontend with React + Typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,48 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path')
 
 module.exports = {
-  entry: './app/javascripts/app.js',
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.js'
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
-  plugins: [
-    // Copy our app's index.html to the build folder.
-    new CopyWebpackPlugin([
-      { from: './app/index.html', to: "index.html" }
-    ])
-  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.tsx', '.ts', '.json', '.sol']
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: path.join(__dirname, '/dist/'),
+    inline: true,
+    host: '0.0.0.0',
+    port: 8080
+  },
   module: {
     rules: [
       {
-       test: /\.css$/,
-       use: [ 'style-loader', 'css-loader' ]
-      }
-    ],
-    loaders: [
-      { test: /\.json$/, use: 'json-loader' },
-      {
-        test: /\.js$/,
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-          plugins: ['transform-runtime']
-        }
+     },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.sol$/,
+        use: [
+            {loader: 'web3-loader'},
+            {loader: 'solc-loader'}
+        ]
       }
     ]
   }
